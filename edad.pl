87626597ef691b031b95841bc15be337900aea4c
juanmi1994 <32478760+juanmi1994@users.noreply.github.com>
2017-11-28 13:09:27 +0100

    Update edad.pl

--- edad.pl
+++ edad.pl
@@ -1,3 +1,4 @@
+
 #!/usr/bin/perl
 use utf8;
 print "Content-Type: text/html\n\n";
@@ -7,11 +8,11 @@ if($ENV{'QUERY_STRING'} eq "") {
          print "<input name=\"Edad\" value=\"Joven\" type=\"radio\">1 entre 20<br>";
          print "<input name=\"Edad\" value=\"Adulto\" type=\"radio\">21 entre 40<br>";
          print "<input name=\"Edad\" value=\"Anciano\" type=\"radio\">41 o mas<br>";
-            print "<input type=\"submit\" value=\"Enviar\">";
+            print "<input type=\"submit\" value=\"Enviar formulario\">";
 }
 
 else {
-  @arGet=split("=",$ENV{'QUERY_STRING'});
-  print "Eres $arGet[1] Disfruta";
+   print "<h1>Felicidades!!</h1>"; 
+	  @arGet=split("=",$ENV{'QUERY_STRING'});
+ 		 print "Eres $arGet[1] Disfruta";
 }
-
