2c61eea155a10d802e1fc6b40a1d4c067f501989
juanmi1994 <32478760+juanmi1994@users.noreply.github.com>
2017-11-28 13:01:42 +0100

    Update edad.pl

--- edad.pl
+++ edad.pl
@@ -3,11 +3,11 @@ use utf8;
 print "Content-Type: text/html\n\n";
 if($ENV{'QUERY_STRING'} eq "") {
    print "<h1>Introduce tu edad</h1>";
-   print "<form name=\"search\" method=\"get\" >";
-   print "<input name=\"Edad\" value=\"Joven\" type=\"radio\">1-20<br>";
-   print "<input name=\"Edad\" value=\"Adulto\" type=\"radio\">21-40<br>";
-   print "<input name=\"Edad\" value=\"Anciano\" type=\"radio\">41 o mas<br>";
-   print "<input type=\"submit\" value=\"Enviar\">";
+      print "<form name=\"search\" method=\"get\" >";
+         print "<input name=\"Edad\" value=\"Joven\" type=\"radio\">1 entre 20<br>";
+         print "<input name=\"Edad\" value=\"Adulto\" type=\"radio\">21 entre 40<br>";
+         print "<input name=\"Edad\" value=\"Anciano\" type=\"radio\">41 o mas<br>";
+            print "<input type=\"submit\" value=\"Enviar\">";
 }
 
 else {
